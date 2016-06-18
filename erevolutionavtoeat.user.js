6eb62bc4bb385ba070213d3cb5647fd607515bb6
nime11 <lena.rit@gmail.com>
2016-06-18 19:45:15 +0200

    Update erevolutionavtoeat.user.js

--- erevolutionavtoeat.user.js
+++ erevolutionavtoeat.user.js
@@ -13,9 +13,18 @@
 // ==/UserScript==
 
 //auto eat
-setInterval(myMethod, 50000);
+var rec=50000;
+var htmlStringdiv= "<li class='active'><span><p style='text-align: center;'> <button onclick='myFunction()'>SCRIPPT</button></p> </span></li>";
+ $(htmlStringdiv).insertAfter(".nav-header nav-header-first hidden-sm");
+ 
+ function myFunction() {
+     rec = prompt("Please enter your time of recover", "5");
+
+}
+ 
+ setInterval(myMethod, rec);
 
 function myMethod( )
 {
-  document.getElementById("energyButton").click()
+  document.getElementById("energyButton").click();
 }
