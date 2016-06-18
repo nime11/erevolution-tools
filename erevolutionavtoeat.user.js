c2449ba527d3cf366e2c79b9ae80598b26ccbacd
nime11 <lena.rit@gmail.com>
2016-06-18 20:52:06 +0200

    Update erevolutionavtoeat.user.js

--- erevolutionavtoeat.user.js
+++ erevolutionavtoeat.user.js
@@ -13,16 +13,23 @@
 // ==/UserScript==
 
 //auto eat
-var rec=50000;
+var a = $('#energyTime').data('original-title');
+var m = parseInt( a[25]);
+var s = parseInt( a[27]+a[28]);
+var rec=10;
+var k=(m*60+ s) * 100;
+var cas=k*rec;
 var htmlStringdiv= "<li class='active'><span><p style='text-align: center;'> <button onclick='myFunction()'>SCRIPPT</button></p> </span></li>";
- $(htmlStringdiv).insertAfter(".nav-header nav-header-first hidden-sm");
+ $(htmlStringdiv).insertAfter(".nav-header-first");
  
  function myFunction() {
-     rec = prompt("Please enter your time of recover", "5");
-
+     rec =  prompt("Please enter your how much food you want to recover at once", "10");
+     cas=k*rec;
+     console.log(cas);
+      setInterval(myMethod, cas);
 }
  
- setInterval(myMethod, rec);
+ setInterval(myMethod, cas);
 
 function myMethod( )
 {
