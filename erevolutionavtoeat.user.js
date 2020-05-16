f489ef490ee2919cafd2223a1694dc1c585b92ec
nime11 <lena.rit@gmail.com>
2020-05-16 09:50:50 +0200

    Update erevolutionavtoeat.user.js

--- erevolutionavtoeat.user.js
+++ erevolutionavtoeat.user.js
@@ -11,17 +11,6 @@
 // @exclude     https://www.erevollution.com/en/special-items
 
 // ==/UserScript==
-
-//auto eat
-var a = $('#energyTime').data('original-title');
-var m = parseInt( a[25]);
-var s = parseInt( a[27]+a[28]);
-var rec=10;
-var k=(m*60+ s) * 100;
-var cas=k*rec;
-var htmlStringdiv= "<li class='active'><span><p style='text-align: center;'> <button onclick='myFunction()'>SCRIPPT</button></p> </span></li>";
- $(htmlStringdiv).insertAfter(".nav-header-first");
- 
  function myFunction() {
      rec =  prompt("Please enter your how much food you want to recover at once", "10");
      cas=k*rec;
@@ -35,3 +24,14 @@ function myMethod( )
 {
   document.getElementById("energyButton").click();
 }
+//auto eat
+var a = $('#energyTime').data('original-title');
+var m = parseInt( a[25]);
+var s = parseInt( a[27]+a[28]);
+var rec=10;
+var k=(m*60+ s) * 100;
+var cas=k*rec;
+var htmlStringdiv= "<li class='active'><span><p style='text-align: center;'> <button onclick='myFunction()'>SCRIPPT</button></p> </span></li>";
+ $(htmlStringdiv).insertAfter(".nav-header-first");
+ 
+
